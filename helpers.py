59aff23b9d3553a640fc56fedb18cd2fc1c466d5
Luis Hernandez <luisrnandezc@gmail.com>
2023-11-12 15:24:16 -0400

    Added helpers.py for dataframe interpolation

---
+++ helpers.py
@@ -0,0 +1,14 @@
+"""
+Cessna 172N Performance Calculator
+Author: Luis Hernández
+GitHub: luisrnandezc
+Date: 13/09/2023
+"""
+
+
+def linear_interpolation():
+    return None
+
+
+def dataframe_interpolation():
+    return None
