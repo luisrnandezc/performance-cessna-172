0c41ed89aad81c1ce85bb9ba1855e65c279b09c5
Luis Hernandez <luisrnandezc@gmail.com>
2023-11-30 10:30:29 -0400

    output.py working properly

--- main.py
+++ main.py
@@ -15,50 +15,52 @@ import output
 import pandas as pd
 import openpyxl
 
-# Read input data.
-input_path = r'C:\Users\luish\Desktop\Projects\Performance Cessna 172N\input\case1.txt'
-input_file = open(input_path, 'r')
 
-# Check input data.
-input_data = input.check_input_data(input_file)
+if __name__ == '__main__':
+    # Read input data.
+    input_path = r'C:\Users\luish\Desktop\Projects\Performance Cessna 172N\input\case1.txt'
+    input_file = open(input_path, 'r')
 
-# Store performance data.
-performance_path = r'C:\Users\luish\Desktop\Projects\Performance Cessna 172N\data\performance_data.xlsx'
-takeoff_df = pd.read_excel(performance_path, sheet_name='takeoff')
-roc_df = pd.read_excel(performance_path, sheet_name='roc', index_col=0)
-climb_df = pd.read_excel(performance_path, sheet_name='climb', index_col=0)
-power_df = pd.read_excel(performance_path, sheet_name='power')
-range40_df = pd.read_excel(performance_path, sheet_name='range40', index_col=0)
-range50_df = pd.read_excel(performance_path, sheet_name='range50', index_col=0)
-endurance40_df = pd.read_excel(performance_path, sheet_name='endurance40', index_col=0)
-endurance50_df = pd.read_excel(performance_path, sheet_name='endurance50', index_col=0)
-landing_df = pd.read_excel(performance_path, sheet_name='landing', index_col=1)
-atm_df = pd.read_excel(performance_path, sheet_name='atm', index_col=1)
+    # Check input data.
+    input_data = input.check_input_data(input_file)
 
-# Generate valid performance data.
-valid_data = data.compute_valid_performance_data(input_data, power_df)
+    # Store performance data.
+    performance_path = r'C:\Users\luish\Desktop\Projects\Performance Cessna 172N\data\performance_data.xlsx'
+    takeoff_df = pd.read_excel(performance_path, sheet_name='takeoff')
+    roc_df = pd.read_excel(performance_path, sheet_name='roc', index_col=0)
+    climb_df = pd.read_excel(performance_path, sheet_name='climb', index_col=0)
+    power_df = pd.read_excel(performance_path, sheet_name='power')
+    range40_df = pd.read_excel(performance_path, sheet_name='range40', index_col=0)
+    range50_df = pd.read_excel(performance_path, sheet_name='range50', index_col=0)
+    endurance40_df = pd.read_excel(performance_path, sheet_name='endurance40', index_col=0)
+    endurance50_df = pd.read_excel(performance_path, sheet_name='endurance50', index_col=0)
+    landing_df = pd.read_excel(performance_path, sheet_name='landing', index_col=1)
+    atm_df = pd.read_excel(performance_path, sheet_name='atm', index_col=1)
 
-# Compute takeoff performance.
-takeoff_ground_roll, takeoff_fifty_ft_roll, roc = takeoff.compute_takeoff_performance(input_data, takeoff_df, roc_df)
+    # Generate valid performance data.
+    valid_data = data.compute_valid_performance_data(input_data, power_df)
 
-# Compute cruise performance.
-if input_data['FC'] == 40:
-    range_df = range40_df
-    endurance_df = endurance40_df
-else:
-    range_df = range50_df
-    endurance_df = endurance50_df
-max_endurance, max_range, velocity, fuel_flow = cruise.compute_cruise_performance(input_data, power_df, range_df, endurance_df)
+    # Compute takeoff performance.
+    takeoff_ground_roll, takeoff_fifty_ft_roll, roc = takeoff.compute_takeoff_performance(input_data, takeoff_df, roc_df)
 
-# Compute fuel required.
-fuel_required, fuel_reserve = fuel.compute_fuel_required(input_data, climb_df, velocity, fuel_flow)
+    # Compute cruise performance.
+    if input_data['FC'] == 40:
+        range_df = range40_df
+        endurance_df = endurance40_df
+    else:
+        range_df = range50_df
+        endurance_df = endurance50_df
+    max_endurance, max_range, velocity, fuel_flow = cruise.compute_cruise_performance(input_data, power_df, range_df, endurance_df)
 
-# Compute landing performance.
-land_ground_roll, land_fifty_roll = landing.compute_landing_performance(input_data, landing_df)
+    # Compute fuel required.
+    fuel_required, fuel_reserve = fuel.compute_fuel_required(input_data, climb_df, velocity, fuel_flow)
 
-# Generate performance file.
-results = [takeoff_ground_roll, takeoff_fifty_ft_roll,
-           max_endurance, max_range, fuel_required, fuel_reserve,
-           land_ground_roll, land_fifty_roll]
-output_data = output.write_output_file(results)
-print('foo')
+    # Compute landing performance.
+    land_ground_roll, land_fifty_roll = landing.compute_landing_performance(input_data, landing_df)
+
+    # Generate performance file.
+    results = [takeoff_ground_roll, takeoff_fifty_ft_roll,
+               max_endurance, max_range, fuel_required, fuel_reserve,
+               land_ground_roll, land_fifty_roll]
+    output_data = output.write_output_file(results)
+    print('foo')
--- output.py
+++ output.py
@@ -5,30 +5,23 @@ GitHub: luisrnandezc
 Date: 13/09/2023
 """
 
-text_for_file = ['Takeoff ground roll: ',
-                 'Takeoff ground roll 50 ft obstacle: ',
-                 'Endurance: ',
-                 'Range: ',
-                 'Fuel required: ',
-                 'Fuel reserve: ',
-                 'Landing ground roll: ',
-                 'Landing ground roll 50 ft obstacle: ']
-
 
 def organize_final_results(results_values):
-    results_keys = ['TOGR', 'TOGR50', 'END', 'RAN', 'FRQ', 'FR', 'LDGR', 'LDGR50']
+    results_keys = ['TOGR [ft]', 'TOGR50 [ft]', 'Endurance [hours]', 'Range [nm]', 'Fuel [gal]', 'Reserve [gal]', 'LDGR [ft]', 'LDGR50 [ft]']
     results = {}
     for (key, value) in zip(results_keys, results_values):
         results[key] = value
     return results
 
 
-def generate_text_file(text, results):
-    # TODO
+def generate_text_file(results):
+    with open(r'C:\Users\luish\Desktop\Projects\Performance Cessna 172N\output\output.txt', 'w') as f:
+        for key, value in results.items():
+            f.write('%s: %s\n' % (key, value))
     return None
 
 
 def write_output_file(results_values):
     results = organize_final_results(results_values)
-    # file = generate_text_file(text_for_file, results)
+    generate_text_file(results)
     return None
