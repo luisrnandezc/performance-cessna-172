d262a4ac47bd95c2e4f8f7edc690cdbd3d8f1d66
Luis Hernandez <luisrnandezc@gmail.com>
2024-08-12 17:46:45 -0400

    feat: enhance input-data form to include wind data fields
    Add three new fields in the performance-data input form; runway heading,
    cruise heading and wind direction. This automates the computation of wind direction/intensity,
    thereby reducing the workload for users and improving results accuracy.
    
    This change aims to streamline the data entry process and enhance overall usability.

--- config/performance/forms.py
+++ config/performance/forms.py
@@ -29,6 +29,8 @@ class PerformanceData(forms.Form):
     fuel_capacity = forms.ChoiceField(widget=forms.RadioSelect, choices=TANK_VOLUME)
 
     # Takeoff fields.
+    to_rwy = forms.IntegerField(min_value=1, max_value=36,
+                                   widget=forms.TextInput(attrs={'placeholder': 'Takeoff runway number (ex: 7)'}))
     to_length = forms.IntegerField(min_value=0,
                                    widget=forms.TextInput(attrs={'placeholder': 'Takeoff runway length in ft'}))
     to_condition = forms.ChoiceField(widget=forms.RadioSelect, choices=RUNWAY_CONDITION)
@@ -38,21 +40,27 @@ class PerformanceData(forms.Form):
                                  widget=forms.TextInput(attrs={'placeholder': 'Takeoff temperature in °C'}))
     to_wind_speed = forms.IntegerField(min_value=0, max_value=35,
                                        widget=forms.TextInput(attrs={'placeholder': 'Takeoff wind speed in kts'}))
-    to_wind_direction = forms.ChoiceField(widget=forms.RadioSelect, choices=WIND_DIRECTION)
+    to_wind_direction = forms.IntegerField(min_value=1, max_value=360,
+                                           widget=forms.TextInput(attrs={'placeholder': 'Takeoff wind direction (ex: 040)'}))
 
     # Cruise fields.
     travel_dist = forms.IntegerField(min_value=0, max_value=750,
                                      widget=forms.TextInput(attrs={'placeholder': 'Travel distance in nm'}))
+    cr_heading = forms.IntegerField(min_value=1, max_value=36,
+                                    widget=forms.TextInput(attrs={'placeholder': 'Cruise heading (ex: 090)'}))
     cr_press_alt = forms.IntegerField(min_value=0, max_value=14200,
                                       widget=forms.TextInput(attrs={'placeholder': 'Cruise pressure altitude in ft'}))
     cr_temp = forms.IntegerField(min_value=-20, max_value=40,
                                  widget=forms.TextInput(attrs={'placeholder': 'Cruise temperature in °C'}))
     cr_wind_speed = forms.IntegerField(min_value=0, max_value=50,
                                        widget=forms.TextInput(attrs={'placeholder': 'Cruise wind speed in kts'}))
-    cr_wind_direction = forms.ChoiceField(widget=forms.RadioSelect, choices=WIND_DIRECTION)
+    cr_wind_direction = forms.IntegerField(min_value=1, max_value=360,
+                                           widget=forms.TextInput(attrs={'placeholder': 'Cruise wind direction (ex: 120)'}))
     cr_power = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Cruise rpm'}))
     
     # Landing fields.
+    land_rwy = forms.IntegerField(min_value=1, max_value=36,
+                                  widget=forms.TextInput(attrs={'placeholder': 'Landing runway number (ex: 27)'}))
     land_length = forms.IntegerField(min_value=0,
                                      widget=forms.TextInput(attrs={'placeholder': 'Landing runway length in ft'}))
     land_condition = forms.ChoiceField(widget=forms.RadioSelect, choices=RUNWAY_CONDITION)
@@ -62,7 +70,8 @@ class PerformanceData(forms.Form):
                                    widget=forms.TextInput(attrs={'placeholder': 'Landing temperature in °C'}))
     land_wind_speed = forms.IntegerField(min_value=0, max_value=35,
                                          widget=forms.TextInput(attrs={'placeholder': 'Landing wind speed in kts'}))
-    land_wind_direction = forms.ChoiceField(widget=forms.RadioSelect, choices=WIND_DIRECTION)
+    land_wind_direction = forms.IntegerField(min_value=1, max_value=360,
+                                             widget=forms.TextInput(attrs={'placeholder': 'Landing wind direction (ex: 180)'}))
     
     # General data validation.
     def clean_to_weight(self):
@@ -72,6 +81,12 @@ class PerformanceData(forms.Form):
         return data
 
     # Takeoff data validation.
+    def clean_to_rwy(self):
+        data = self.cleaned_data['to_rwy']
+        if data < 1 or data > 36:
+            raise ValidationError(_('Invalid value - The runway number must be between 1 and 36'))
+        return data
+
     def clean_to_length(self):
         data = self.cleaned_data['to_length']
         if data <= 0:
@@ -96,11 +111,23 @@ class PerformanceData(forms.Form):
             raise ValidationError(_('Invalid value - The wind speed must be between 0 and 35 knots'))
         return data
 
+    def clean_to_wind_direction(self):
+        data = self.cleaned_data['to_wind_direction']
+        if data < 1 or data > 360 or data % 5 != 0:
+            raise ValidationError(_('Invalid value - The wind direction must be a multiple of 5 between 1 and 360'))
+        return data
+
     # Cruise data validation.
     def clean_travel_dist(self):
         data = self.cleaned_data['travel_dist']
         if data < 0 or data > 750:
             raise ValidationError(_('Invalid value - The distance must be between 0 and 750 nautical miles'))
+        return data
+
+    def clean_cr_heading(self):
+        data = self.cleaned_data['cr_heading']
+        if data < 1 or data > 360 or data % 5 != 0:
+            raise ValidationError(_('Invalid value - The heading must be a multiple of 5 between 1 and 360'))
         return data
 
     def clean_cr_press_alt(self):
@@ -121,6 +148,12 @@ class PerformanceData(forms.Form):
             raise ValidationError(_('Invalid value - The wind speed must be between 0 and 50 knots'))
         return data
 
+    def clean_cr_wind_direction(self):
+        data = self.cleaned_data['cr_wind_direction']
+        if data < 1 or data > 360 or data % 5 != 0:
+            raise ValidationError(_('Invalid value - The wind direction must be a multiple of 5 between 1 and 360'))
+        return data
+
     def clean_cr_power(self):
         data = self.cleaned_data['cr_power']
         if data < 2100 or data > 2650:
@@ -128,6 +161,12 @@ class PerformanceData(forms.Form):
         return data
 
     # Landing validation data.
+    def clean_land_rwy(self):
+        data = self.cleaned_data['land_rwy']
+        if data < 1 or data > 36:
+            raise ValidationError(_('Invalid value - The runway number must be between 1 and 36'))
+        return data
+
     def clean_land_length(self):
         data = self.cleaned_data['land_length']
         if data <= 0:
@@ -150,5 +189,11 @@ class PerformanceData(forms.Form):
         data = self.cleaned_data['land_wind_speed']
         if data < 0 or data > 35:
             raise ValidationError(_('Invalid value - The wind speed must be between 0 and 35 knots'))
+        return data
+
+    def clean_land_wind_direction(self):
+        data = self.cleaned_data['land_wind_direction']
+        if data < 1 or data > 360 or data % 5 != 0:
+            raise ValidationError(_('Invalid value - The wind direction must be a multiple of 5 between 1 and 360'))
         return data
         
