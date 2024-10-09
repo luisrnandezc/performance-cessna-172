aaede8911fd804294690d3f52161b2d04049c026
Luis Hernandez <luisrnandezc@gmail.com>
2024-10-08 22:33:44 -0400

    chore: remove commented block of code

--- config/performance/views.py
+++ config/performance/views.py
@@ -51,31 +51,5 @@ def process_csv(csv_file):
             performance_data[field] = int(value)
         else:
             performance_data[field] = value
-    # if all(field in performance_data for field in fields):
-    #     UploadCSVData.objects.create(
-    #         to_weight=int(performance_data['to_weight']),
-    #         fuel_capacity=int(performance_data['fuel_capacity']),
-    #         to_heading=int(performance_data['to_heading']),
-    #         to_length=int(performance_data['to_length']),
-    #         to_condition=performance_data['to_condition'],
-    #         to_press_alt=int(performance_data['to_press_alt']),
-    #         to_temp=int(performance_data['to_temp']),
-    #         to_wind_speed=int(performance_data['to_wind_speed']),
-    #         to_wind_direction=int(performance_data['to_wind_direction']),
-    #         travel_dist=int(performance_data['travel_dist']),
-    #         cr_heading=int(performance_data['cr_heading']),
-    #         cr_press_alt=int(performance_data['cr_press_alt']),
-    #         cr_temp=int(performance_data['cr_temp']),
-    #         cr_wind_speed=int(performance_data['cr_wind_speed']),
-    #         cr_wind_direction=int(performance_data['cr_wind_direction']),
-    #         cr_power=int(performance_data['cr_power']),
-    #         land_heading=int(performance_data['land_heading']),
-    #         land_length=int(performance_data['land_length']),
-    #         land_condition=performance_data['land_condition'],
-    #         land_press_alt=int(performance_data['land_press_alt']),
-    #         land_temp=int(performance_data['land_temp']),
-    #         land_wind_speed=int(performance_data['land_wind_speed']),
-    #         land_wind_direction=int(performance_data['land_wind_direction'])
-    #     )
 
     return performance_data
