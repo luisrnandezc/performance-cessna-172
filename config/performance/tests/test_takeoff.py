ad7f22637133c9bd14a362d9eda8e5f7ffeca955
Luis Hernandez <luisrnandezc@gmail.com>
2024-10-10 22:00:20 -0400

    add tests folder and initial test file

--- config/performance/tests/test_takeoff.py
+++ config/performance/tests/test_takeoff.py
@@ -1,11 +1,8 @@
-import sys
-import os
 import unittest
 import pandas as pd
+from src.takeoff import compute_takeoff_ground_roll
 
-
-
-performance_path = r'C:\Users\luish\Desktop\Projects\performance_cessna_172\config\performance\src\data'
+performance_path = r'/config/performance/src/data'
 takeoff_df = pd.read_csv(performance_path + '\\' + 'takeoff.csv')
 roc_df = pd.read_csv(performance_path + '\\' + 'roc.csv', index_col=0)
 
@@ -13,7 +10,7 @@ roc_df = pd.read_csv(performance_path + '\\' + 'roc.csv', index_col=0)
 class TestTakeoff(unittest.TestCase):
 
     def test_compute_takeoff_ground_roll(self):
-        result1, result2 = takeoff.compute_takeoff_ground_roll(2300, 0, 0, takeoff_df)
+        result1, result2 = compute_takeoff_ground_roll(2300, 0, 0, takeoff_df)
         obtained = (int(result1), int(result2))
         expected = (720, 1300)
         self.assertEqual(obtained, expected)
